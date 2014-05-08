1726501f095a3eab8855fdce3291b3c2c340d6d4
Ryan Wahle <ryanwahle@fullsail.edu>
2014-05-07 22:28:09 -0700

    removed menu and completed basic interface

--- LinearLayout/app/src/main/java/com/ryanwahle/linearlayout/app/MainActivity.java
+++ LinearLayout/app/src/main/java/com/ryanwahle/linearlayout/app/MainActivity.java
@@ -1,10 +1,34 @@
+/*
+Ryan Wahle
+Full Sail University
+Java 1 1405
+Linear Layout Assignment
+ */
+
+/*
+Scratch Pad:
+
+o  int or float variable
+o  boolean variable
+o  String variable
+o  loop (any type)
+o  condition
+o  function
+o  click event handler
+
+The application must also utilize, at minimum, 1 of each of the following Android components:
+
+x  Android resource value
+x  TextView
+x  EditText
+x  Button
+x  LinearLayout
+ */
+
 package com.ryanwahle.linearlayout.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class MainActivity extends Activity {
 
@@ -13,26 +37,4 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
